230db2739395585aae1f7bb651d95ae288680044
blindmelon07 <bryandetera07@gmail.com>
2026-01-07 01:45:50 -0800

    roles

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
@@ -17,6 +18,10 @@ class UserForm
                 TextInput::make('email')
                     ->label('Email address')
                     ->email()
+                    ->required(),
+                Select::make('role_id')
+                    ->relationship('roles', 'name')
+                    ->multiple()
                     ->required(),
                 DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
